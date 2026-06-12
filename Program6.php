d8c0c7e845694eaa83e6ffee66f7f087b0311de2
Meet Dinesh Jadav <meetjadav115@gmail.com>
2026-06-12 12:43:21 +0530

    Add files via upload

---
+++ Program6.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<b>Using While Loop:</b><br>";
+
+$i=15;
+
+while($i<=20){
+    echo "<b>$i</b><br>";
+    $i++;
+}
+
+echo "<b>Using Do While Loop:</b><br>";
+$j=15;
+do{
+    echo $j."<br/>";
+    $j++;
+}while($j<=20); 
+
+
+?>
