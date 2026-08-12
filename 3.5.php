a38f2fa2e30cad1be47fe78dc1b73f50e4a25b03
Meet Dinesh Jadav <meetjadav115@gmail.com>
2026-08-12 10:22:42 +0530

    Add files via upload

---
+++ 3.5.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+       Username:<input type="text" name="username">
+       <br></br>
+       Password:<input type="text" name="password">
+       <br></br>
+       <input type="submit" value="Login">
+       <br></br>
+    </form>
+</body>
+</html>
+
+
+<?php
+
+    session_start();
+
+    if ($_SERVER['REQUEST_METHOD']=="POST") {  
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        $_SESSION['name']=$user;
+        header('location:homepage.php');     
+
+    }   
+?>
