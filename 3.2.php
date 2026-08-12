325dc1f45ca76a0f14342d2580562108bb7494f4
Meet Dinesh Jadav <meetjadav115@gmail.com>
2026-08-12 10:21:38 +0530

    Add files via upload

---
+++ 3.2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+       <input type="submit" value="Naam kadho">
+       <br></br>
+    </form>
+</body>
+</html>
+
+
+<?php
+
+    if ($_SERVER['REQUEST_METHOD']=="POST") {  
+      
+        echo "Username-" . $_COOKIE["username"]."<br>";        
+    }   
+?>
